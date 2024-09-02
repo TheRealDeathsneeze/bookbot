013ce045b9ed82a3907c47a88e9b633534261618
TheRealDeathsneeze <16346809+TheRealDeathsneeze@users.noreply.github.com>
2024-09-02 12:25:24 -0500

    added input prompts for various functions, added comments

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 # bookbot project for boot.dev by beccaKay
-
+#  
 def main(): 
     #change to ask for input or pass as arg later
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     word_count = get_num_words(text)
     chars_dict = get_char_dict(text)
-    print(f"word count: {word_count}")
-    print(chars_dict)
+    
+    
+    print(text) if query("Read Book Text?") == True else None
+    print(f"word count: {word_count}") if query("Word count?") == True else None
+    print(chars_dict) if query("Character dictionary?") == True else None
+
+############################################################
+# Main Functionality of bookbot goes here
+############################################################
 
 #count independent characters and return a dictionary
 def get_char_dict(text):
@@ -31,7 +38,14 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+#prompt user for Y/n input, return true/false
 
+def query(question):
+    print(f"{question} Y/n?")
+    choice = input().lower()
+    if choice == "y":
+        return True
+    return False
 
 main()
 
