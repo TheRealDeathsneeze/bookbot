bb3b3c7358f8ec5a839a80c02e96979f0fe3ea63
BeccaKay <16346809+TheRealDeathsneeze@users.noreply.github.com>
2024-09-05 09:13:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,39 @@ def main():
     chars_dict = get_char_dict(text)
     
     
-    print(text) if query("Read Book Text?") == True else None
-    print(f"word count: {word_count}") if query("Word count?") == True else None
-    print(chars_dict) if query("Character dictionary?") == True else None
+    #print(text) if query("Read Book Text?") == True else None
+    #print(f"word count: {word_count}") if query("Word count?") == True else None
+    #print(chars_dict) if query("Character dictionary?") == True else None
+
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
 ############################################################
 # Main Functionality of bookbot goes here
